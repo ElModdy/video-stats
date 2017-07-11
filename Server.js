af6657289b56f57e0e0fd3616aa4cf897112cdf3
Modesto Davide <modesto.davide@gmail.com>
2017-07-11 17:04:13 +0200

    Simple static http server implementetion

--- Server.js
+++ Server.js
@@ -1,29 +1,35 @@
-
 var download = require('download-pdf');
 var pdf2Text = require('pdf2text');
 var moment = require('moment');
+var express = require('express');
+
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(3002, function() {
+  console.log('Server listening!');
+});
 
 var anno = "2017";
 var pista = "GER";
 var campionato = "MotoGP";
 var sessione = "RAC";
 
-var pdfURL = "http://resources.motogp.com/files/results/" + anno + "/" +
-                                                            pista + "/" +
-                                                            campionato + "/" +
-                                                            sessione + "/Session.pdf";
-var pathToPdf = __dirname + "/Stats.pdf";
+var details = [anno,
+               pista,
+               campionato,
+               sessione];
+
+var pdfURL = "http://resources.motogp.com/files/results/" + details.join('/') + "/Session.pdf";
+
+var pathToPdf = "./pdfs/" + details.join('_') + ".pdf";
 
 var regTime = /RACE START [0-9]+:[0-9]+'[0-9]+/;
 var regDay = /\w+ \d+, \d+/;
 var dateFormat = "MMMM DD, YYYY HH:mm'ss";
 
-var options = {
-    directory: ".",
-    filename: "Stats.pdf"
-}
-
-download(pdfURL, options, function(err){
+download(pdfURL, {filename: pathToPdf}, function(err){
     pdf2Text(pathToPdf).then(function(pages) {
       var firstPage = pages[0].join(" ");
       var milliDate = moment(firstPage.match(regDay)[0] + " " +
@@ -32,4 +38,4 @@ download(pdfURL, options, function(err){
                              true).format('x');
       console.log(milliDate);
     });
-})
+});
