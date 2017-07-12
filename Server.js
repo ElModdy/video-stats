43467cc0af83a638797f4125962ea8906bf0d750
Davide Modesto <modesto.davide@gmail.com>
2017-07-12 20:36:28 +0200

    Ajax response implemented

--- Server.js
+++ Server.js
@@ -8,43 +8,30 @@ var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
-/*app.get('/', function(req, res) {
-
-   request('http://www.google.com', function (error, response, body) {
-      console.log('body:', body);
-   });
-   res.send('hello world');
-});*/
-
-app.listen(3002, function() {
-  console.log('Server listening!');
-});
-
-var anno = "2017";
-var pista = "GER";
-var campionato = "MotoGP";
-var sessione = "RAC";
-
-var details = [anno,
-               pista,
-               campionato,
-               sessione];
-
-var pdfURL = "http://resources.motogp.com/files/results/" + details.join('/') + "/Session.pdf";
-
-var pathToPdf = "./pdfs/" + details.join('_') + ".pdf";
+var pathToPdf = "./pdfs/Session.pdf";
 
 var regTime = /RACE START [0-9]+:[0-9]+'[0-9]+/;
 var regDay = /\w+ \d+, \d+/;
 var dateFormat = "MMMM DD, YYYY HH:mm'ss";
 
-download(pdfURL, {filename: pathToPdf}, function(err){
-    pdf2Text(pathToPdf).then(function(pages) {
-      var firstPage = pages[0].join(" ");
-      var milliDate = moment(firstPage.match(regDay)[0] + " " +
-                             firstPage.match(regTime)[0].substring(11, 19),
-                             dateFormat,
-                             true).format('x');
-      console.log(milliDate);
-    });
+app.get('/gettimestamp', function(req, res){
+  var pdfURL = "http://resources.motogp.com/files/results/" + req.query.anno +
+                                                              req.query.pista +
+                                                              req.query.campionato +
+                                                              req.query.sessione +
+                                                              "/Session.pdf";
+  download(pdfURL, {filename: pathToPdf}, function(err){
+      pdf2Text(pathToPdf).then(function(pages) {
+        var firstPage = pages[0].join(" ");
+        var milliDate = moment(firstPage.match(regDay)[0] + " " +
+                               firstPage.match(regTime)[0].substring(11, 19),
+                               dateFormat,
+                               true).format('x');
+        res.json(milliDate);
+      });
+  });
+});
+
+app.listen(3001, function() {
+  console.log('Server listening!');
 });
