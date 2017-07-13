2659a530e8ad1aa3d7919bbb39d49bda43d44012
Modesto Davide <modesto.davide@gmail.com>
2017-07-13 12:29:49 +0200

    Fixed reading pdfs

--- Server.js
+++ Server.js
@@ -3,6 +3,7 @@ var pdf2Text = require('pdf2text');
 var moment = require('moment');
 var express = require('express');
 var request = require('request');
+var fs = require('fs');
 
 var app = express();
 
@@ -20,32 +21,32 @@ function Nodo(value){
 }
 
 Nodo.prototype.addChild = function(child) {
-   this.childs.push(child);
+  this.childs.push(child);
 }
 
 var root = new Nodo(null);
 
 function cerca(details, level, nodo){
-   var childs = nodo.childs;
-   if(level == 4){
-      return childs[0].value;
-   }
-   for(var i=0, len = childs.length; i < len; i++){
-      if (childs[i].value == details[level]) {
-         return cerca(details, ++level, childs[i]);
-      }
-   }
-   return [level,nodo];
+  var childs = nodo.childs;
+  if(level == 4){
+    return childs[0].value;
+  }
+  for(var i=0, len = childs.length; i < len; i++){
+    if (childs[i].value == details[level]) {
+      return cerca(details, ++level, childs[i]);
+    }
+  }
+  return [level,nodo];
 }
 
 function add(details, value, level, nodo){
-   if(level == 4){
-      nodo.addChild(new Nodo(value));
-   }else{
-      var child = new Nodo(details[level]);
-      nodo.addChild(child);
-      add(details, value, ++level, child);
-   }
+  if(level == 4){
+    nodo.addChild(new Nodo(value));
+  }else{
+    var child = new Nodo(details[level]);
+    nodo.addChild(child);
+    add(details, value, ++level, child);
+  }
 }
 
 //EXAMPLE URL: http://localhost:3001/gettimestamp?anno=2017&pista=GER&campionato=MotoGP&sessione=RAC
@@ -60,24 +61,26 @@ app.get('/gettimestamp', function(req, res){
 
   var result = cerca(details, 0, root);
   if(result.constructor === Array){
-     var pdfURL = "http://resources.motogp.com/files/results/" + details.join("/") + "/Session.pdf";
+    var pdfURL = "http://resources.motogp.com/files/results/" + details.join("/") + "/Session.pdf";
 
-     download(pdfURL, {filename: pathToPdf}, function(err){
-         pdf2Text(pathToPdf).then(function(pages) {
-           var firstPage = pages[0].join(" ");
-           var milliDate = parseInt(moment(firstPage.match(regDay)[0] + " " +
-                                    firstPage.match(regTime)[0].substring(11, 19),
-                                    dateFormat,
-                                    true).format('x'));
-           add(details, milliDate, result[0], result[1]);
-           res.json(milliDate);
-         });
-     });
+    download(pdfURL, {filename: pathToPdf}, function(){
+      var buffer = fs.readFileSync(pathToPdf)
+      pdf2Text(buffer).then(function(pages) {
+        var firstPages = pages[0].concat(pages[1]).join(" ");
+        var milliDate = parseInt(moment(firstPages.match(regDay)[0] + " " +
+                                        firstPages.match(regTime)[0]
+                                        .substring(11, 19),
+                                        dateFormat,
+                                        true).format('x'));
+        add(details, milliDate, result[0], result[1]);
+        res.json(milliDate);
+      });
+    });
   }else{
     res.json(result);
   }
 });
 
-app.listen(3001, function() {
+app.listen(3000, function() {
   console.log('Server listening!');
 });
